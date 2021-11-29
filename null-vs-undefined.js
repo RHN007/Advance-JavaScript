52a631a148527d1a9540776047c070fa74884c70
Rakibul Hassan Nayon <rakibul.nayon@outlook.com>
2021-11-29 07:29:29 +0600

    correcting few things

--- null-vs-undefined.js
+++ null-vs-undefined.js
@@ -1,26 +1,25 @@
 //How you will get undefined
 
-let pakhi; (by giving without value)
+let tomato; //(by giving without value)
 
 function add(num1, num2) {
   console.log(num1 + num2);
   return;
 }
 
- function add(num1, num2) {
+function add(num1, num2) {
   console.log(num1, num2);
 }
 
-const premik = { name: "Smart dude", phone: 01712222222 };
-console.log(premik.gf);
+const student = { name: "Smart dude", phone: 01712222222 };
+console.log(student.gf);
 
 let fun = undefined;
 console.log(fun);
 
 let ages = [2, 5, 9];
 
-console.log(ages[11
-]);
+console.log(ages[11]);
 
-//null means blank usually developers set null where 
-//undefined can be found in many ways in above . 
+//null means blank usually developers set null where
+//undefined can be found in many ways in above .
