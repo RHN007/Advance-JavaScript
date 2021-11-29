7324f94d34276ed6b5f4e0a88e24f439bd91351b
Rakibul Hassan Nayon <rakibul.nayon@outlook.com>
2021-11-29 10:10:55 +0600

    scope details how to learn about scope

---
+++ scope.js
@@ -0,0 +1,4 @@
+//let and constant is a scope variable. Which works under only within bracket .
+//this is why people use let and const rather that using
+//hoisting/ block scope / global scope ki oita Importat ;
+//thatsdude.com (Scope ) related one of the most important topic to understand .
