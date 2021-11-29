c4d7aa1b9b238eaf5083002749b76f76c6f4b9b8
Rakibul Hassan Nayon <rakibul.nayon@outlook.com>
2021-11-29 07:37:45 +0600

    Double vs triple equals

--- double-vs-triple-equals.js
+++ double-vs-triple-equals.js
@@ -1,8 +1,17 @@
 const first = 2;
-const second = 3;
+const second = "2"; //string
 
 if (first == second) {
-  console.log("condition is true");
+  console.log("Condition is True");
 } else {
   console.log("condition is false");
 }
+
+if (first === second) {
+  console.log("condition is true");
+} else {
+  console.log("Condition is False");
+}
+
+//Double equals only checks value ; (o && false = true, 1 && true = true)
+// Triple equals checks value and types as well. (better to use triple equals for beginners level developers)
